b49fc78c4eb6c900c08a37b0b8904adddf2618c1
Logan Keenan <d.logankeenan@gmail.com>
2019-11-29 12:53:55 -0600

    add handlebars as the templating language

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse};
+use handlebars::Handlebars;
+use std::collections::BTreeMap;
 
-fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
+fn root(_req: HttpRequest) -> HttpResponse {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".hbs", "./src/templates")
+        .unwrap();
 
-    format!("Hello {}!", &name)
+    let mut data = BTreeMap::new();
+    data.insert("paragraph_text", "This page was rendered using Actix-Web as the web server and handlebars as the templating language!");
+
+    let body = handlebars.render("index", &data).unwrap();
+
+    HttpResponse::Ok().body(body)
 }
 
 fn main() {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/", web::get().to(root))
     })
         .bind("127.0.0.1:8000")
         .expect("Can not bind to port 8000")
         .run()
         .unwrap();
 }
-
